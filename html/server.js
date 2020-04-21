d2d7b116acb403839caf95fc2def12dee8e91e05
LuzGonzalez <luzalgonzalezro@ittepic.edu.mx>
2020-04-21 12:10:51 -0600

    serve static files

--- html/server.js
+++ html/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+
 const app = express();
 
-/*Definir una ruta inicial para que sea esta la
-que responda con un documento HTML y luego la función
-manejadora que recibe 2 argumentos uno con la información de
-la petición y otro con lo necesario para la respuesta*/
+
+app.use('/assets',express.static('assets'));
+
 app.get('/',function(req,res){
   res.sendFile('index.html',{
     root: __dirname
